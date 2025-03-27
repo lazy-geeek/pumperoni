5806643ad819e2f6ecc5a462622ec0b39aa35b15
lazy-geeek <lazy-geek@outlook.com>
2025-03-27 13:36:00 +0100

    gmgn and pump.fun removed

--- market_trends.py
+++ market_trends.py
@@ -1,17 +1,30 @@
 import requests
-from config import GMGN_API, PUMPFUN_API
+from config import DEXSCREENER_API_BASE
 
 
 # Market trend tracking
 def fetch_trends():
     try:
-        gmgn_response = requests.get(GMGN_API.format(token="trends"))
-        pumpfun_response = requests.get(PUMPFUN_API)
-        gmgn_trends = gmgn_response.json() if gmgn_response.status_code == 200 else []
-        pumpfun_trends = (
-            pumpfun_response.json() if pumpfun_response.status_code == 200 else []
+        dexscreener_url = f"{DEXSCREENER_API_BASE}/token-boosts/latest/v1"
+        dexscreener_response = requests.get(dexscreener_url)
+        dexscreener_data = (
+            dexscreener_response.json()
+            if dexscreener_response.status_code == 200
+            else []
         )
-        return gmgn_trends + pumpfun_trends
+
+        # Format the data
+        dexscreener_trends = [
+            {
+                "chainId": token["chainId"],
+                "tokenAddress": token["tokenAddress"],
+                "url": token["url"],
+                "icon": token["icon"],
+            }
+            for token in dexscreener_data
+        ]
+
+        return dexscreener_trends
     except requests.RequestException as e:
         print(f"Error fetching trends: {e}")
         return []
